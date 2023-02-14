19ce0374011e98e968677b82e3b67e017cf4deb6
ealvarado <erick.alvarado@cit.hn>
2023-02-14 16:42:11 -0600

    'avance'

---
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -0,0 +1,5 @@
+package alpha.yalo.tr;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'alpha.yalo.tr',
+  appName: 'Alpha',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
